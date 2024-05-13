8f5c042f72f5071a6aba12da0c81b4f0c37942b3
NurRus <102742407+NurRus@users.noreply.github.com>
2024-05-13 19:17:04 +0300

    13_05-1

---
+++ src/s21_exp.c
@@ -0,0 +1,20 @@
+#include "s21_math.h"
+
+long double s21_exp(double x) {
+  long double answer = 0;
+  if (x > 709 || x < -1000) {
+    answer = (x > 709) ? s21_INF : 0;
+  } else if (x < 0) {
+    answer = 1 / s21_exp((-1) * x);
+  } else {
+    for (int k = 0; k < 200; ++k) {
+      double current = 1;
+      for (int j = 1; j <= k; ++j) {
+        current *= x;
+        current /= j;
+      }
+      answer += current;
+    }
+  }
+  return answer;
+}
